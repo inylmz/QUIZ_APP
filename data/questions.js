c3c7d2135bb86a7e057dee2e7fb11a30d9316da6
İREMNUR YILMAZ <122210098+inylmz@users.noreply.github.com>
2024-03-29 14:31:03 +0300

    QUIZ_APP
    Siber Güvenlik Quiz Uygulaması

---
+++ data/questions.js
@@ -0,0 +1,110 @@
+
+export default [
+    {
+        question: "Aşağıdakilerden hangisi ağ cihazlarından değildir?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Hub",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Telgraf",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Router",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Switch",
+            },
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "OSI modeli kaç katmandan oluşur? ",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"4",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"3",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"8",
+            },
+            {
+                id:"3",
+                options:"D",
+                answer:"7",
+            },
+        ],
+        correctAnswerIndex: 3,
+
+    },
+    {
+        question: "OSI modeli kaç katmandan oluşur? ",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"4",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"3",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"8",
+            },
+            {
+                id:"3",
+                options:"D",
+                answer:"7",
+            },
+        ],
+        correctAnswerIndex: 3,
+
+    },
+    {
+        question: "OSI modeli kaç katmandan oluşur? ",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"4",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"3",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"8",
+            },
+            {
+                id:"3",
+                options:"D",
+                answer:"7",
+            },
+        ],
+        correctAnswerIndex: 3,
+
+    }
+]
---
+++ screens/HomeScreen.js
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+  return (
+    <View style={{ marginTop: 15 }}>
+      <Image
+        style={{ height: 370, width: "100%", resizeMode: "contain" }}
+        source={require('../cyber.png') } 
+      />
+
+      <View style={{ padding: 10 }}>
+        <Text
+          style={{
+            textAlign: "center",
+            color: "mediumblue",
+            fontSize: 20,
+            fontWeight: "600",
+          }}
+        >
+          QUIZ KURALLARI
+        </Text>
+
+        <View
+          style={{
+            padding: 10,
+            backgroundColor: "#F88379",
+            borderRadius: 6,
+            marginTop: 15,
+          }}
+        >
+          
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 5,
+            }}
+          >
+            <Text style={{ color: "white" }}>•</Text>
+            <Text
+              style={{
+                marginLeft: 4,
+                color: "#722F37",
+                fontSize: 15,
+                fontWeight: "500",
+              }}
+            >
+              Her sorunun 15 saniyelik bir zaman sınırı vardır.
+            </Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 5,
+            }}
+          >
+            <Text style={{ color: "white" }}>•</Text>
+            <Text
+              style={{
+                marginLeft: 4,
+                color: "#722F37",
+                fontSize: 15,
+                fontWeight: "500",
+              }}
+            >
+              Tüm soruları zorunlu olarak cevaplamalısınız.
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <Pressable
+      onPress={() => navigation.navigate("Quiz")}
+        style={{
+          backgroundColor: "mediumblue",
+          padding: 14,
+          width:120,
+          borderRadius: 25,
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: 30,
+        }}
+      >
+        <Text style={{color:"white",fontWeight:"600",textAlign:"center"}}>Start Quiz</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
