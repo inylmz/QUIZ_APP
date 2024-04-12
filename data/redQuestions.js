0f39d4dcb4e9e9c284dbcc0fcda15e2f42fa3aec
İREMNUR YILMAZ <iremnurylmz007@gmail.com>
2024-04-12 14:11:09 +0300

    yeni soru eklendi

---
+++ data/redQuestions.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      question: "Kırmızının RGB değeri nedir?",
+      options: [
+        { id: "0", options: "A", answer: "(255, 0, 0)" },
+        { id: "1", options: "B", answer: "(0, 255, 0)" },
+        { id: "2", options: "C", answer: "(0, 0, 255)" },
+        { id: "3", options: "D", answer: "(128, 0, 0)" },
+      ],
+      correctAnswerIndex: 0,
+    },
+    {
+      question: "En yaygın kırmızı renk tonu hangisidir ?",
+      options: [
+        { id: "0", options: "A", answer: "Maroon" },
+        { id: "1", options: "B", answer: "Crimson" },
+        { id: "2", options: "C", answer: "Scarlet" },
+        { id: "3", options: "D", answer: "Ruby" },
+      ],
+      correctAnswerIndex: 1,
+    }
+  ];
+  
