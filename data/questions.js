e6f23c3e5dc946d720fbf87a6a96080eb48e2f38
İREMNUR YILMAZ <122210098+inylmz@users.noreply.github.com>
2024-04-25 22:30:40 +0300

    yeni sorular eklendi

--- data/questions.js
+++ data/questions.js
@@ -1,23 +1,142 @@
 export default [
-    {
-      question: "Kırmızının RGB değeri nedir?",
-      options: [
-        { id: "0", options: "A", answer: "(255, 0, 0)" },
-        { id: "1", options: "B", answer: "(0, 255, 0)" },
-        { id: "2", options: "C", answer: "(0, 0, 255)" },
-        { id: "3", options: "D", answer: "(128, 0, 0)" },
-      ],
-      correctAnswerIndex: 0,
-    },
-    {
-      question: "En yaygın kırmızı renk tonu hangisidir?",
-      options: [
-        { id: "0", options: "A", answer: "Maroon" },
-        { id: "1", options: "B", answer: "Crimson" },
-        { id: "2", options: "C", answer: "Scarlet" },
-        { id: "3", options: "D", answer: "Ruby" },
-      ],
-      correctAnswerIndex: 1,
-    }
-  ];
-  
+  {
+    question: "Security Information and Event Management (SIEM) nedir?",
+    options: [
+      { id: "0", option: "A", answer: "Bir güvenlik tehdit türü" },
+      { id: "1", option: "B", answer: "Güvenlik olaylarını izlemek, analiz etmek ve yanıtlamak için kullanılan bir sistem" },
+      { id: "2", option: "C", answer: "Bir siber güvenlik cihazı" },
+      { id: "3", option: "D", answer: "Bir saldırı tespit sistemi" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Security Orchestration, Automation, and Response (SOAR) hangi işlemleri birleştirir?",
+    options: [
+      { id: "0", option: "A", answer: "Saldırı analizi ve yanıt" },
+      { id: "1", option: "B", answer: "Otomasyon ve yanıtlandırma süreçleri" },
+      { id: "2", option: "C", answer: "Veri analizi ve raporlama" },
+      { id: "3", option: "D", answer: "Güvenlik güçlendirme ve izleme" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Incident Response Plan'in amacı nedir?",
+    options: [
+      { id: "0", option: "A", answer: "Organizasyonun güvenlik politikalarını belirlemek" },
+      { id: "1", option: "B", answer: "Güvenlik olaylarına hızlı ve etkili bir şekilde yanıt vermek" },
+      { id: "2", option: "C", answer: "Ağ trafiğini izlemek ve analiz etmek" },
+      { id: "3", option: "D", answer: "Güvenlik testleri yapmak" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Threat Intelligence neyi içerir?",
+    options: [
+      { id: "0", option: "A", answer: "Güvenlik politikalarını" },
+      { id: "1", option: "B", answer: "Güvenlik tehditlerini izlemek, anlamak ve bunlara karşı korunmak için kullanılan bilgileri" },
+      { id: "2", option: "C", answer: "Güvenlik olaylarını" },
+      { id: "3", option: "D", answer: "Saldırı tespit sistemlerini" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Vulnerability Management nedir?",
+    options: [
+      { id: "0", option: "A", answer: "Güvenlik politikalarını belirlemek" },
+      { id: "1", option: "B", answer: "Sistemlerdeki zayıflıkları tespit etmek, değerlendirmek ve düzeltmek için kullanılan süreçler" },
+      { id: "2", option: "C", answer: "Güvenlik olaylarını izlemek" },
+      { id: "3", option: "D", answer: "Güvenlik olaylarına yanıt vermek" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Security Operations Center (SOC) ne iş yapar?",
+    options: [
+      { id: "0", option: "A", answer: "Sistemlerdeki güvenlik açıklarını tespit eder" },
+      { id: "1", option: "B", answer: "Güvenlik olaylarını izler, tespit eder, yanıtlar ve raporlar" },
+      { id: "2", option: "C", answer: "Saldırıları engeller" },
+      { id: "3", option: "D", answer: "Güvenlik politikalarını oluşturur" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Endpoint Security nedir?",
+    options: [
+      { id: "0", option: "A", answer: "Ağ izleme" },
+      { id: "1", option: "B", answer: "Bilgisayar ağlarında kullanılan cihazların güvenliğini sağlamak için kullanılan önlemler ve teknolojiler" },
+      { id: "2", option: "C", answer: "Güvenlik olaylarına yanıt vermek" },
+      { id: "3", option: "D", answer: "Veri analizi" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "User and Entity Behavior Analytics (UEBA) ne işe yarar?",
+    options: [
+      { id: "0", option: "A", answer: "Saldırıları engeller" },
+      { id: "1", option: "B", answer: "Kullanıcı ve varlık davranışlarını izler, analiz eder ve anormal davranışları tespit eder" },
+      { id: "2", option: "C", answer: "Veri analizi" },
+      { id: "3", option: "D", answer: "Güvenlik olaylarını izler" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Identity and Access Management (IAM) ne iş yapar?",
+    options: [
+      { id: "0", option: "A", answer: "Güvenlik politikalarını oluşturur" },
+      { id: "1", option: "B", answer: "Kullanıcıların kimliklerini doğrular, erişim haklarını yönetir ve denetler" },
+      { id: "2", option: "C", answer: "Sistemlerdeki zayıflıkları tespit eder" },
+      { id: "3", option: "D", answer: "Saldırıları engeller" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Security Incident and Event Management (SIEM) hangi işlevleri sağlar?",
+    options: [
+      { id: "0", option: "A", answer: "Güvenlik olaylarını izler ve analiz eder" },
+      { id: "1", option: "B", answer: "Güvenlik tehditlerini engeller" },
+      { id: "2", option: "C", answer: "Ağ trafiğini hızlandırır" },
+      { id: "3", option: "D", answer: "Veri analizi yapar" },
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: "Cloud Security hangi tür güvenlik için kullanılır?",
+    options: [
+      { id: "0", option: "A", answer: "Yalnızca ağ güvenliği" },
+      { id: "1", option: "B", answer: "Veri güvenliği" },
+      { id: "2", option: "C", answer: "Bulut bilişim ortamlarında veri güvenliği sağlamak" },
+      { id: "3", option: "D", answer: "Fiziksel güvenlik" },
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: "Secure Coding Practices neyi hedefler?",
+    options: [
+      { id: "0", option: "A", answer: "Yazılım geliştirme sürecini hızlandırmak" },
+      { id: "1", option: "B", answer: "Geliştirme sürecinde güvenlik açıklarını azaltmak" },
+      { id: "2", option: "C", answer: "Yazılım testlerini zorlaştırmak" },
+      { id: "3", option: "D", answer: "Yazılım geliştirme sürecini karmaşık hale getirmek" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Cyber Threat Hunting ne iş yapar?",
+    options: [
+      { id: "0", option: "A", answer: "Sistemlerdeki zayıflıkları tespit eder" },
+      { id: "1", option: "B", answer: "Gizli tehditleri tespit eder ve proaktif bir şekilde hareket eder" },
+      { id: "2", option: "C", answer: "Güvenlik olaylarını yanıtlandırır" },
+      { id: "3", option: "D", answer: "Veri analizi yapar" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Data Loss Prevention (DLP) neyi amaçlar?",
+    options: [
+      { id: "0", option: "A", answer: "Veri analizi" },
+      { id: "1", option: "B", answer: "Hassas verilerin izlenmesi, kontrol edilmesi ve korunması" },
+      { id: "2", option: "C", answer: "Bulut bilişim" },
+      { id: "3", option: "D", answer: "Sistem güncellemeleri" },
+    ],
+    correctAnswerIndex: 1,
+  }
+];
